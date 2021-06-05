93e15854ab434269f59d21c36a4e9b94c6db9006
Mark <dev@html42.de>
2021-06-05 15:32:36 +0200

    User Class optimazation

--- x4/plugin_users/classes/xtreme_user.class.php
+++ x4/plugin_users/classes/xtreme_user.class.php
@@ -5,6 +5,7 @@ class XtremeUser {
     public $id = null;
     public $username = null;
     public $from_db = null;
+    public static $CACHE = array('instances' => array());
 
     public function __construct($userid) {
         if (is_numeric($userid)) {
@@ -13,12 +14,28 @@ class XtremeUser {
             if (isset($this->from_db['username'])) {
                 $this->username = trim($this->from_db['username']);
             }
+            self::$CACHE['instances'][$this->id] = $this;
         }
     }
-    
+
+    /**
+     * @param int $id
+     * @return XtremeUser
+     */
+    public static function load($id) {
+        if (is_numeric($id)) {
+            $id = intval($id);
+            if (!isset(self::$CACHE['instances'][$id])) {
+                self::$CACHE['instances'][$id] = new self($id);
+            }
+            return self::$CACHE['instances'][$id];
+        }
+        return null;
+    }
+
     public function in_group($name) {
-        foreach($this->from_db['groups'] as $group) {
-            if($group['name'] == $name) {
+        foreach ($this->from_db['groups'] as $group) {
+            if ($group['name'] == $name) {
                 return true;
             }
         }
